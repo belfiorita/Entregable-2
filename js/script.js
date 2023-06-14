754fafdb91c54a7cf79ec93d67142da9293c7bcb
belfiorita <agostina.belfiori@gmail.com>
2023-06-14 10:26:14 -0300

    FIX CONSOLE ISSUES

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Creation of global const, vars and class
 
 const digitalArtworks = [];
-const file = "../data/data.json";
+const file = "./data/data.json";
 const myForm = document.getElementById("myForm");
 const shop = document.getElementById("shop");
 const modal = document.getElementById("modalPopUp");
